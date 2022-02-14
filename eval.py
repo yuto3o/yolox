fa3fdc8791ed8e7505b177933889eaef69c7d885
jdufour <julien.dufour@inria.fr>
2022-02-14 13:44:41 +0000

    🐛 Fix eval

--- eval.py
+++ eval.py
@@ -1,11 +1,17 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
+from absl import app, flags
+
 from core.utils import decode_cfg, load_weights
-from core.callbacks import COCOEvalCheckpoint, VOCEvalCheckpoint
+from core.callbacks import VOCEvalCheckpoint
 
-if __name__ == '__main__':
+flags.DEFINE_string('config', '', 'path to config file')
+FLAGS = flags.FLAGS
 
-    cfg = decode_cfg('cfgs/coco_yolov3_tiny.yaml')
+
+def main(_argv):
+    print('Config File From:', FLAGS.config)
+    cfg = decode_cfg(FLAGS.config)
 
     model_type = cfg['yolo']['type']
     if model_type == 'yolov3':
@@ -20,18 +26,24 @@ if __name__ == '__main__':
     elif model_type == 'yolov4_tiny':
         from core.model.one_stage.yolov4 import YOLOv4_Tiny as Model
 
+    elif model_type == 'yolox':
+        from core.model.one_stage.custom import YOLOX as Model
+
     else:
         raise NotImplementedError()
 
     model, eval_model = Model(cfg)
     model.summary()
 
-    init_weight = cfg["train"]["init_weight_path"]
+    init_weight = cfg["test"]["init_weight_path"]
     load_weights(model, init_weight)
 
     eval_callback = VOCEvalCheckpoint(save_path=None,
                                       eval_model=eval_model,
                                       model_cfg=cfg,
-                                      sample_rate=1,
                                       verbose=1)
     eval_callback.on_epoch_end(0)
+
+
+if __name__ == '__main__':
+    app.run(main)
